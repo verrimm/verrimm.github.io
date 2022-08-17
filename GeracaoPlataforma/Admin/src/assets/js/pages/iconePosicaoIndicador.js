cdf39b2c0f433667faba26de89efc3df82cb1c72
Matheus Verri <107270021+verrimm@users.noreply.github.com>
2022-08-17 12:54:37 -0300

    página do indicador

--- GeracaoPlataforma/Admin/src/assets/js/pages/iconePosicaoIndicador.js
+++ GeracaoPlataforma/Admin/src/assets/js/pages/iconePosicaoIndicador.js
@@ -1,24 +1,36 @@
+var posicao = document.querySelectorAll(".iconePosicaoRanking")
+switch (posicao[0].textContent) {
+    case "1º":
+        posicao[0].classList.add("bg-success")
+        break;
 
+    case "2º":
+        posicao[0].classList.add("bg-success")
+        break;
 
-      var posicao = document.querySelectorAll(".iconePosicaoRanking")
-      
-      
-      switch (posicao[0].textContent) {
-        case "1º":
-            posicao[0].classList.add("bg-success")
-            break;
+    case "3º":
+        posicao[0].classList.add("bg-success")
+        break;
 
-        case "2º":
-            posicao[0].classList.add("bg-success")
-            break;
+    default:
+        posicao[0].classList.add("bg-danger")
+        break;
 
-        case "3º":
-            posicao[0].classList.add("bg-success")
-            break;
-        
-        default:
-            posicao[0].classList.add("bg-danger")
-            break;
-            
+}
 
-      }
+//Função para esconder card de regra na página do indicador
+function hideShow() {
+    var card = document.getElementById("cardRegrasCalculo");
+    var texto = document.getElementById("textoDescricao");
+    var icone = document.getElementById("iconDescricao");
+
+    if (card.style.display === "none") {
+        card.style.display = "block";
+        texto.innerHTML = "Esconder Descrição";
+        icone.classList.replace("bx-show", "bx-hide")
+    } else {
+        card.style.display = "none";
+        texto.innerHTML = "Mostrar Descrição";
+        icone.classList.replace("bx-hide", "bx-show");
+    }
+}
